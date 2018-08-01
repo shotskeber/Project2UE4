74d13a4461338e0331f6e5748ae875852d18a5d6
shotskeber <italo.pardave@gmail.com>
2018-07-31 22:53:24 -0500

    TankActorPawn

---
+++ TankProject/Source/TankProject/MyTankProjectGameMode_BP.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TankProjectGameModeBase.h"
+#include "MyTankProjectGameMode_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKPROJECT_API AMyTankProjectGameMode_BP : public ATankProjectGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
